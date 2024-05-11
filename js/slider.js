2756200399fc1d8fdbb24c2a2b0e0855e0528248
espinozaje <jeisson12aaron@gmail.com>
2024-05-11 13:59:19 -0500

    coawmidmawoidwa

--- js/slider.js
+++ js/slider.js
@@ -41,7 +41,7 @@
     
     prev.addEventListener('click', function(){
         let items = document.querySelectorAll('.item')
-        document.querySelector('.slide').prepend(items[items.length - 1]) // here the length of items = 6
+        document.querySelector('.slide').prepend(items[items.length - 1]) 
     })
     
     
